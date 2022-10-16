bc1f4c659b9534e8047406b3868c85f2e9b14908
ggpabuk <59561857+ggpabuk@users.noreply.github.com>
2022-10-16 16:34:51 +0300

    (fix) extra dots removed

--- ExtSpoofer/Program.cs
+++ ExtSpoofer/Program.cs
@@ -30,11 +30,11 @@ namespace ExtSpoofer
                     break;
             
                 string trueExtension = Path.GetExtension(openFile.FileName).TrimStart('.');
-                string fakeExtension = Path.GetExtension(saveFile.FileName).TrimStart('.');
+                string fakeExtension = Path.GetExtension(saveFile.FileName);
                 string saveBase = saveFile.FileName.Replace(fakeExtension, String.Empty);
 
                 File.Copy(openFile.FileName, 
-                    $"{saveBase}\u202e{ReverseString(fakeExtension)}.{trueExtension}");
+                    saveBase + '\u202e' + ReverseString(fakeExtension) + trueExtension);
 
                 Console.Write("Press any key to continue...");
                 Console.ReadKey(false);
